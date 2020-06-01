64f15655db6a1e2c953951901059f2285395e97d
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-06-01 11:19:01 +0300

    Добавил исполняемый метод

--- IndGraphCykl/IndGraphCykl/Form1.cs
+++ IndGraphCykl/IndGraphCykl/Form1.cs
@@ -19,6 +19,28 @@ namespace IndGraphCykl
             InitializeComponent();
         }
 
+        bool dfs(int key, int[,] a, int[] was, int n)
+        {
+            was[key] = 1;
+            for (int i = 0; i < n; i++)
+            {
+                if (a[key,i]!=0)
+                {
+                    if (was[i] == 0)
+                    {
+                        if (dfs(i,a,was,n))
+                            return true;
+                    }
+                    else if (was[i] == 1)
+                    {
+                        return true;
+                    }
+                }
+            }
+            was[key] = 2;
+            return false;
+        }
+
         private void Run_Click(object sender, EventArgs e)
         {
             OpenFileDialog openGraph = new OpenFileDialog();
@@ -41,8 +63,7 @@ namespace IndGraphCykl
             string temp = graphMatrix.Text;
             double t = Math.Sqrt((temp.Length + 1) / 2);
             int n = (int)t;
-
-
+            
             string[] matrInfo = temp.Split(' ', '\n');
             int[,] matrData = new int[n, n];
             int q = 0;
