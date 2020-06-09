83171a268ffbe9a0043ffc41c4f5a614325e5184
DeadDDolphin <62338390+DeadDDolphin@users.noreply.github.com>
2020-06-09 04:16:35 +0300

    Ну, поп-пытки  алгоритма Куна

---
+++ Алгоритм Куна/Алгоритм Куна/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Алгоритм_Куна
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Run = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.parosoch = new System.Windows.Forms.RichTextBox();
+            this.matrCraph = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // Run
+            // 
+            this.Run.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.Run.Location = new System.Drawing.Point(337, 198);
+            this.Run.Name = "Run";
+            this.Run.Size = new System.Drawing.Size(109, 48);
+            this.Run.TabIndex = 0;
+            this.Run.Text = "==>";
+            this.Run.UseVisualStyleBackColor = true;
+            this.Run.Click += new System.EventHandler(this.Run_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(114, 109);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(111, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Матрица смежности";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(551, 109);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Наибольшее паросочетание";
+            // 
+            // parosoch
+            // 
+            this.parosoch.Location = new System.Drawing.Point(538, 140);
+            this.parosoch.Name = "parosoch";
+            this.parosoch.Size = new System.Drawing.Size(180, 180);
+            this.parosoch.TabIndex = 3;
+            this.parosoch.Text = "";
+            // 
+            // matrCraph
+            // 
+            this.matrCraph.Location = new System.Drawing.Point(80, 140);
+            this.matrCraph.Name = "matrCraph";
+            this.matrCraph.Size = new System.Drawing.Size(180, 180);
+            this.matrCraph.TabIndex = 4;
+            this.matrCraph.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.matrCraph);
+            this.Controls.Add(this.parosoch);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Run);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Run;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.RichTextBox parosoch;
+        private System.Windows.Forms.RichTextBox matrCraph;
+    }
+}
+
---
+++ Алгоритм Куна/Алгоритм Куна/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Алгоритм_Куна
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        bool dfs(int v, bool[] used, int[,] g, int size, int[] matching)
+        {
+            if (used[v])
+                return false;
+            used[v] = true;
+            for (int i = 0; i < size; i++)
+            {
+                int t = g[v, i];
+                if (matching[t] == -1 || dfs(matching[t], used, g, size, matching))
+                {
+                    matching[t] = v;
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        string Kun(int[,]g, int size)
+        {
+            int []matching = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                matching[i] = -1;
+            }
+            bool[] used = new bool[size];
+
+            for (int i = 0;i<size;i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    used[j] = false;
+                }
+                dfs(i, used, g, size, matching);
+            }
+            string temp = "";
+            for (int i = 0; i < size; i++)
+            {
+                if (matching[i] != -1)
+                    temp += i + "->" + matching[i] + "\n";
+            }
+            return temp;
+        }
+
+        private void Run_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openGraph = new OpenFileDialog();
+
+            openGraph.InitialDirectory = "C:\\Users\\Dima\\Documents\\GitHub\\Kostruir\\Алгоритм Куна\\";
+            openGraph.Filter = "txt files (*.txt)|*.txt";
+            openGraph.FilterIndex = 2;
+            openGraph.RestoreDirectory = true;
+
+            if (matrCraph.Text == "")
+            {
+                if (openGraph.ShowDialog() == DialogResult.OK)
+                {
+                    string filePath = openGraph.FileName;
+                    string text = File.ReadAllText(filePath);
+                    matrCraph.AppendText(text);
+                }
+            }
+
+            string temp = matrCraph.Text;
+            double t = Math.Sqrt((temp.Length + 1) / 2);
+            int n = (int)t;
+
+            string[] temp2 = temp.Split(' ', '\n');
+            int[,] matrData = new int[n, n];
+            int q = 0;
+            for (int j = 0; j < n; j++)
+            {
+                for (int k = 0; k < n; k++)
+                {
+                    matrData[j, k] = Int32.Parse(temp2[q]);
+                    q++;
+                }
+            }
+
+            temp = Kun(matrData, n);
+            parosoch.AppendText(temp);
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "txt files |*.txt";
+            if (saveFileDialog1.ShowDialog() == System.Windows.Forms.DialogResult.OK && saveFileDialog1.FileName.Length > 0)
+            {
+
+                using (FileStream fstream = new FileStream(saveFileDialog1.FileName, FileMode.OpenOrCreate))
+                {
+
+                    fstream.SetLength(0);
+                    byte[] array = System.Text.Encoding.Default.GetBytes(temp);
+                    fstream.Write(array, 0, array.Length);
+                }
+
+                MessageBox.Show("Сохранено", "Завершение работы", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            else
+            {
+                MessageBox.Show("Ошибка. Повторите попытку", "Завершение работы", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            saveFileDialog1.FileName = "";
+        }
+    }
+}
